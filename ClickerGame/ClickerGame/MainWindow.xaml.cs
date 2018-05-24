596c2220598a2c163d90a1ae138d7c4e59c25d2b
Nerdy Wizard <magicmanandy@gmail.com>
2018-05-24 17:47:35 +0100

    Button Animation
    Button animates on click to indicate when it can be used again. Will resize (kind of) when window is resized.

--- ClickerGame/ClickerGame/MainWindow.xaml.cs
+++ ClickerGame/ClickerGame/MainWindow.xaml.cs
@@ -9,10 +9,12 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+
 
 namespace ClickerGame
 {
@@ -23,26 +25,48 @@ namespace ClickerGame
     {
         Int32 wood;
         DispatcherTimer timer;
+        DoubleAnimation buttonWidthAnimation;
 
         public MainWindow()
         {
             InitializeComponent();
-            timer = new DispatcherTimer();
-            timer.Interval = new TimeSpan(0, 0, 2);
-            timer.Tick += Timer_Tick;
+            this.Loaded += OnWindowLoaded;
         }
 
-        private void Timer_Tick(Object sender, EventArgs e)
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(2) };
+            timer.Tick += Timer_Tick;
+            this.SizeChanged += OnWindowSizeChanged;
+
+            buttonWidthAnimation = new DoubleAnimation
+            {
+                From = 0,
+                To = WoodButton.ActualWidth,
+                Duration = timer.Interval,
+                EasingFunction = new QuarticEase(),
+                FillBehavior = FillBehavior.Stop
+            };
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
         {
             WoodButton.IsEnabled = true;
             timer.Stop();
         }
 
-        private void Button_Click(Object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             WoodDisplay.Text = (++wood).ToString();
             WoodButton.IsEnabled = false;
             timer.Start();
+            WoodButton.BeginAnimation(WidthProperty, buttonWidthAnimation);
+        }
+
+        private void OnWindowSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            WoodButton.Width = e.NewSize.Width - 8;
+            buttonWidthAnimation.To = e.NewSize.Width - 8;
         }
     }
 }
